38600734a0af5b60c49bb6205370622d515a3a96
Arjooohn <90732392+Arjooohn@users.noreply.github.com>
2022-05-09 16:32:01 +0800

    Update trigger1.sql

--- Database Project/trigger1.sql
+++ Database Project/trigger1.sql
@@ -1,5 +1,4 @@
 
-
 --Trigger - every time the user will make an INSERT statement into the Employee table, it will trigger this trigger to check if the 
 -- format of the Employee_ID is correctly written.
 
@@ -19,28 +18,62 @@ END;
 
 --SELECT * FROM Employee;
 
-
 --2 Stored Procedures
 
 --This Stored Procedure will select all the customer in a particular region
-CREATE PROCEDURE SelectAllCustomer @Region_ID VARCHAR(8)
-AS SELECT * FROM Customer WHERE Region_ID = @Region_ID 
-GO;
+CREATE OR REPLACE 
+PROCEDURE budgetcost_diff 
+AS 
+BEGIN
+SELECT Project_ID, Project_Description, Approx_Budget, Actual_Cost, Approx_Budget-Actual_Cost AS "Budget and Cost Difference" 
+FROM Project
 
-EXEC SelectAllCustomer @Region_ID = 'Region_ID'; --change Region_ID
+DBMS_OUTPUT.PUT_LINE ('Budget and Cost Difference Report');
+END;
+
+--to execute the stored procedure
+--EXEC budgetcost_diff();
  
---Get all the number of employess in the task skill table
-CREATE PROCEDURE GetNoOfEmployees
-AS BEGIN 
-SELECT No_Of_Employees FROM Task_Skills
-GO;
+--BEGIN
+--budgetcost_diff();
+--END;
 
-EXEC GetNoOfEmployees;
+
+
+
+
+--Get all involved employees in a project_ID
+CREATE OR REPLACE 
+PROCEDURE GetNoOfEmployees 
+AS 
+BEGIN 
+SELECT Employee.Employee_ID, 
+Employee.Employee_Fname||' '||Employee.Employee_Initial||'. '|| Employee.Employee_Lname AS "Employee_Name", 
+Assignment.Project_ID
+FROM Employee, Assignment
+WHERE Empoyee.Employee_ID = Assignment.Employee_ID;
+GROUP BY Assignment.Project_ID;
+
+DBMS_OUTPUT.PUT_LINE ('Data Retrieved');
+END;
+
+--to execute the stored procedure
+--EXEC GetNoOfEmployees;
+
+--to call the procedure
+--BEGIN 
+--GetNoOfEmployee();
+--END;
+
+
+
+
+
 
 
 --2 Create Views
---view the new employees (working less than 2019)
-CREATE VIEW Employee_Is_New AS 
+--view the new employees or junior employees(?) (working less than 2019)
+CREATE VIEW [Employee_Is_New] AS 
 SELECT Employee_ID, Employee_Fname|| '  ' ||Employee_Initial||'. ' ||Employee_Lname AS Employee_Name, Employee.HireDate
 FROM Employee  
 WHERE Employee.Hire_Date > '2019-01-01 00:00:00.000'
@@ -48,11 +81,10 @@ WHERE Employee.Hire_Date > '2019-01-01 00:00:00.000'
 SELECT * FROM Employee_Is_New;
 
 --view the employee ID working as the longest time
-CREATE VIEW Employee_LongestTime AS
+CREATE VIEW [Employee_LongestTime] AS
 AS SELECT Employee_ID, MAX(Hours_Worked)
 FROM Work_Logs
 
 SELECT * FROM Employee_LongestTime;
 
 
---Hindi pa nafifinalize to kasi hindi pa ako ganon kasure kung gagana to 
